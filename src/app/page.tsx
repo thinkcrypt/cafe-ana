dd753b8e1c8fcb3dd7048a39ec472fa980d0f6d2
ashikuzzaman-abir <ashikuzzamanabir@outlook.com>
2024-05-05 17:22:54 +0600

    photo gallery started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Hero from '@/components/homepage/hero/Hero';
 import OurMenu from '@/components/homepage/our-menu/OurMenu';
 import PhotosSection from '@/components/homepage/photos-section/PhotosSection';
 import AboutUsNew from '@/components/homepage/about-us/AboutUsNew';
+import PhotoGalery from '@/components/photo-gallery/PhotoGalery';
 
 export default function Home() {
 	return (
@@ -13,6 +14,7 @@ export default function Home() {
 			<OurMenu />
 			<PhotosSection />
 			<AboutUsNew />
+			<PhotoGalery />
 		</PageLayout>
 	);
 }
